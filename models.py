a28a256a406b1db8ef215187a16db244a6bf04b3
jdmv31 <josuemedina.mv@gmail.com>
2026-06-11 19:37:18 -0400

    esqueleto

---
+++ models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
